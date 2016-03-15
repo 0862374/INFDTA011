ae9e235fdbae3f9aeb866303f0044d2639f640b7
0862374@hr.nl <0862374@hr.nl>
2016-03-15 13:44:28 +0100

    again bugfix

--- INFDTA011/Algorithms/Pearson.cs
+++ INFDTA011/Algorithms/Pearson.cs
@@ -22,7 +22,7 @@ namespace INFDTA011.Algorithms
             {
                 if (ratingTwo.Where(x => x.ArticleId == ratingOne.ElementAt(i).ArticleId).Count() > 0)
                 {
-                    if (ratingOne.ElementAt(i) != null && ratingTwo.ElementAt(i) != null)
+                    if (ratingOne.Count <= i && ratingTwo.ElementAt(i) <= i)
                     { 
                         n++;
                         double x = ratingOne.ElementAt(i).Rating;
